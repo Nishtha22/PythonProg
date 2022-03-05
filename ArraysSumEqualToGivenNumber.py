227fb41ed87cc1616c1d541219227ee2dd2e7549
Nishtha <garg.nishtha22@gmail.com>
2022-03-05 11:41:03 -0600

    Added the program for Array

--- ArraysSumEqualToGivenNumber.py
+++ ArraysSumEqualToGivenNumber.py
@@ -1,10 +1,10 @@
 #Two unsorted array are given. We need to find the sum of all pairs which sum up to x
 import array as arr
 
-
 def sum_pairs(arr1,arr2,x):
     print([(k,x-k) for k in arr1 if (x-k) in arr2])
 
+#Testing the code
 if __name__=="__main__":
     arr1=arr.array('i',[2,1,4,5])
     arr2=arr.array('i',[6,7,8,5,5])
